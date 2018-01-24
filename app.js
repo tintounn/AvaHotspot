2537730d88b4a67a376ae5fd2c38be4787d39bde
tintounn <tintounn@gmail.com>
2018-01-24 21:51:00 +0100

    vlcplayer lib with radio test

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const Socket = require('socket.io-client');
+const VlcPlayer = require('./lib/vlc.lib');
 
 class AvaHotSpot {
   constructor() {
     this.say = this.say.bind(this);
     this.play = this.play.bind(this);
     this.stop = this.stop.bind(this);
+    this.disconnect = this.disconnect.bind(this);
 
     console.log('Starting hotspot...');
     this.startApp();
@@ -12,9 +14,13 @@ class AvaHotSpot {
 
   async startApp() {
     try {
+      await this.initConfig();
       await this.findCore();
       await this.connect();
       await this.setupHandlers();
+
+      this.player = new VlcPlayer(false);
+      this.play("http://cdn.nrjaudio.fm/adwz1/fr/30043/mp3_128.mp3?origine=fluxradios");
 
       console.log('Hotspot started !');
     } catch(err) {
@@ -22,7 +28,13 @@ class AvaHotSpot {
     }
   }
 
-  findCore() { }
+  initConfig() {
+
+  }
+
+  findCore() {
+    return Promise.resolve();
+  }
 
   connect() {
     return new Promise((resolve, reject) => {
@@ -30,6 +42,7 @@ class AvaHotSpot {
         reconnection: false
       });
       this.socket.on('connect', () => resolve());
+      this.socket.on('connect_timeout', () => reject());
     });
   }
 
@@ -37,6 +50,12 @@ class AvaHotSpot {
     this.socket.on('play', this.play);
     this.socket.on('say', this.say);
     this.socket.on('stop', this.stop);
+    this.socket.on('disconnect', this.disconnect);
+  }
+
+  disconnect() {
+    this.player.stop();
+    console.log('Socket disconnected');
   }
 
   say(msg) {
@@ -44,11 +63,11 @@ class AvaHotSpot {
   }
 
   play(url) {
-
+    this.player.play(url);
   }
 
   stop() {
-
+    this.player.stop();
   }
 }
 
